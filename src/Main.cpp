09ff332bf7f35b8e998125afbcff82e3d1674a5b
Dennis Soemers <d.soemers@gmail.com>
2023-04-27 17:50:48 +0200

    init repo

---
+++ src/Main.cpp
@@ -0,0 +1,64 @@
+#include <stddef.h>
+
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace SKSE::stl;
+
+namespace {
+    /**
+     * Setup logging.
+     */
+    void InitializeLogging() {
+        auto path = log_directory();
+        if (!path) {
+            report_and_fail("Unable to lookup SKSE logs directory.");
+        }
+        *path /= PluginDeclaration::GetSingleton()->GetName();
+        *path += L".log";
+
+        std::shared_ptr<spdlog::logger> log;
+        if (IsDebuggerPresent()) {
+            log = std::make_shared<spdlog::logger>("Global", std::make_shared<spdlog::sinks::msvc_sink_mt>());
+        } else {
+            log = std::make_shared<spdlog::logger>(
+                "Global", std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true));
+        }
+
+#ifndef NDEBUG
+        const auto level = spdlog::level::trace;
+#else
+        const auto level = spdlog::level::info;
+#endif
+
+        log->set_level(level);
+        log->flush_on(level);
+
+        spdlog::set_default_logger(std::move(log));
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
+    }
+
+    /**
+     * Initialize the hooks.
+     */
+    void InitializeHooks() {
+        log::trace("Initializing hooks...");
+        log::trace("Hooks initialized.");
+    }
+}  // namespace
+
+/**
+ * This is the main callback for initializing the SKSE plugin, called just before Skyrim runs its main function.
+ */
+SKSEPluginLoad(const LoadInterface* skse) {
+    InitializeLogging();
+
+    auto* plugin = PluginDeclaration::GetSingleton();
+    auto version = plugin->GetVersion();
+    log::info("{} {} is loading...", plugin->GetName(), version);
+
+    Init(skse);
+    InitializeHooks();
+
+    log::info("{} has finished loading.", plugin->GetName());
+    return true;
+}
